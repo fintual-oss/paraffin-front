dbc5b7e0ba1140058a8c606b9531c0951d78cc43
isavega <igvega02@gmail.com>
2022-10-19 11:49:56 -0300

    feat: add graph view to curriculum

--- src/pages/curriculums/[id].js
+++ src/pages/curriculums/[id].js
@@ -1,14 +1,36 @@
+import React from 'react';
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import LearningUnitsSection from '@components/learning-units-section/LearningUnitsSection';
 import { Skeleton } from 'primereact/skeleton';
+import { TabView, TabPanel } from 'primereact/tabview';
 
 function CurriculumPage() {
   const { query, isReady } = useRouter();
   const curriculumId = query.id;
 
+  const Graph = dynamic(() => import('../../components/learning-units-section/Graph'), {
+    ssr: false,
+  });
+
   if (!isReady) return <Skeleton shape="rectangle" width="100%" height="100%" />;
 
-  return <LearningUnitsSection curriculumId={curriculumId} />;
+  return (
+    <div>
+      <TabView>
+        <TabPanel header="Lista Learning Units">
+          <LearningUnitsSection curriculumId={curriculumId} />
+        </TabPanel>
+        <TabPanel header="Grafo Lerning Units">
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ height: 600, width: 800, margin: 15, position: 'relative' }}>
+              <Graph />
+            </div>
+          </div>
+        </TabPanel>
+      </TabView>
+    </div>
+  );
 }
 
 export default CurriculumPage;
