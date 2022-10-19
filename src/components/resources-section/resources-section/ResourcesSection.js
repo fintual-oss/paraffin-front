2fe141160593f71df28a870816ee77d81840db42
ristofer <cristopher.gomez@ug.uchile.cl>
2022-10-19 14:52:48 -0300

    feat(resource-page): create handler to show a sidebar with the resource info when the view resource button is pressed

--- src/components/resources-section/resources-section/ResourcesSection.js
+++ src/components/resources-section/resources-section/ResourcesSection.js
@@ -1,47 +1,51 @@
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import useGet from '@hooks/useGet';
-import { endpoints } from '@utils/endpoints';
-import { Skeleton } from 'primereact/skeleton';
-import { Button } from 'primereact/button';
-import { Panel } from 'primereact/panel';
-import AddNewResourceModal from '@components/resources-section/add-new-resource-modal/AddNewResourceModal';
-import LearningUnitInformation from '@components/resources-section/learning-unit-information/LearningUnitInformation';
-import styles from './ResourcesSection.module.scss';
-import ResourcesScroller from '../resources-scroller/ResourcesScroller';
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import useGet from "@hooks/useGet";
+import { endpoints } from "@utils/endpoints";
+import { Skeleton } from "primereact/skeleton";
+import { Button } from "primereact/button";
+import { Panel } from "primereact/panel";
+import { Sidebar } from "primereact/sidebar";
+import ResourcesList from "@components/resources-section/resources-list/ResourcesList";
+import AddNewResourceModal from "@components/resources-section/add-new-resource-modal/AddNewResourceModal";
+import LearningUnitInformation from "@components/resources-section/learning-unit-information/LearningUnitInformation";
+import styles from "./ResourcesSection.module.scss";
+import ResourcesScroller from "../resources-scroller/ResourcesScroller";
 
 const ResourcesSection = ({ learningUnitId }) => {
   const router = useRouter();
   const [displayBasic, setDisplayBasic] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [activeResource, setActiveResource] = useState(null);
 
   const {
     data: learningUnit,
     isLoading: isLoadingUnit,
     isError: isErrorUnit,
-  } = useGet(endpoints('learningUnit', learningUnitId));
+  } = useGet(endpoints("learningUnit", learningUnitId));
 
   const {
     data: resources,
     isLoading: isLoadingResources,
     isError: isErrorResources,
     mutate: mutateResources,
-  } = useGet(endpoints('learningUnitResources', learningUnitId));
+  } = useGet(endpoints("learningUnitResources", learningUnitId));
 
   if (isLoadingUnit || isLoadingResources) {
     return <Skeleton shape="rectangle" width="100%" height="100%" />;
   }
   if (isErrorUnit || isErrorResources) {
-    return 'error';
+    return "error";
   }
 
   const saveResourceHandler = (bodyValues) => {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: bodyValues,
     };
     fetch(
-      endpoints('learningUnitResources', learningUnitId),
+      endpoints("learningUnitResources", learningUnitId),
       requestOptions
     ).then(() => {
       mutateResources();
@@ -52,6 +56,11 @@ const ResourcesSection = ({ learningUnitId }) => {
   const modalHandlers = {
     onHide: () => setDisplayBasic(false),
     onSave: saveResourceHandler,
+  };
+
+  const resourceViewButtonHandler = (resource) => {
+    setActiveResource(resource);
+    setSidebarVisible(true);
   };
 
   const header = () => {
@@ -81,6 +90,20 @@ const ResourcesSection = ({ learningUnitId }) => {
           />
         )}
         <ResourcesScroller resources={resources} />
+        <ResourcesList
+          resources={resources}
+          learningUnitId={learningUnitId}
+          resourceViewButtonHandler={(resource) =>
+            resourceViewButtonHandler(resource)
+          }
+        />
+        <Sidebar
+          visible={sidebarVisible}
+          position="right"
+          onHide={() => setSidebarVisible(false)}
+        >
+          {activeResource && activeResource.name}
+        </Sidebar>
       </Panel>
     </div>
   );
