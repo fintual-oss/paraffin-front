3971c97090bd67a5ef15822d157f599c3c69ebdc
Pablo Abara <pbabara@uc.cl>
2022-10-20 15:51:41 -0300

    chore(learning-unit-page): add image to assets and delete hardcoded url

--- src/components/resources-section/learning-unit-information/LearningUnitInformation.js
+++ src/components/resources-section/learning-unit-information/LearningUnitInformation.js
@@ -1,20 +1,13 @@
 import { Card } from 'primereact/card';
 import Image from 'next/image';
 import styles from './LearningUnitInformation.module.scss';
+import learningUnitPhoto from '/src/public/LearningUnit.jpg';
 
 const LearningUnitInformation = ({ learningUnit }) => {
   return (
     <Card className={styles.cardFull}>
       <div className={styles.productListItem}>
-        <Image
-          width="235"
-          height="150"
-          className={styles.img}
-          src={
-            "https://img.freepik.com/vector-gratis/mujeres-felices-aprendiendo-idioma-linea-aislado-ilustracion-vectorial-plana-personajes-femeninos-dibujos-animados-que-toman-lecciones-individuales-traves-messenger-concepto-educacion-tecnologia-digital_74855-10088.jpg?w=2000&t=st=1665151875~exp=1665152475~hmac=e788b587fbbe1bdd29332d55dc1a5965dd181962a0ee5719dc83ef09c895f0b4img.freepik.com/vector-gratis/mujeres-felices-aprendiendo-idioma-linea-aislado-ilustracion-vectorial-plana-personajes-femeninos-dibujos-animados-que-toman-lecciones-individuales-traves-messenger-concepto-educacion-tecnologia-digital_74855-10088.jpg?w=2000&t=st=1665151875~exp=1665152475~hmac=e788b587fbbe1bdd29332d55dc1a5965dd181962a0ee5719dc83ef09c895f0b4'"
-          }
-          alt={learningUnit.name}
-        />
+        <Image width="235" height="150" className={styles.img} src={learningUnitPhoto} alt={learningUnit.name} />
         <div className={styles.productListDetail}>
           <div className={styles.productName}>
             <span>Learning unit</span>
--- src/components/resources-section/resources-list-item/ResourcesListItem.js
+++ src/components/resources-section/resources-list-item/ResourcesListItem.js
@@ -7,30 +7,28 @@ import profilePic from '@utils/images/resource.jpg';
 
 const ResourcesListItem = ({ resource }) => {
   return (
-    <div>
-      <div className={styles.resourceGridItem}>
-        <div className={styles.resourceListItem}>
-          <Link href={`/resources/${resource.id}`}>
-            <Image className={styles.img} src={profilePic} alt={resource.name} />
-          </Link>
-          <div className="product-list-detail">
-            <div className={styles.resourceName}>
-              <Link href={`/resources/${resource.id}`}>{resource.name}</Link>
-            </div>
-            <div className={styles.resourceValidation}>
-              <Average average={parseFloat(resource.average_evaluation).toFixed(1)} />
-            </div>
-            <div className={styles.resourceUrl}>
-              <i className="pi pi-link"></i>{' '}
-              <a href={resource.url} target="_blank" rel="noreferrer">
-                Enlace del recurso
-              </a>
-            </div>
-            <div className={styles.resourceLink}>
-              <Link href={`/resources/${resource.id}`}>
-                <Button label="Ver recurso" icon="pi pi-book" className="p-button-outlined p-button-sm" />
-              </Link>
-            </div>
+    <div className={styles.resourceGridItem}>
+      <div className={styles.resourceListItem}>
+        <Link href={`/resources/${resource.id}`}>
+          <Image className={styles.img} src={profilePic} alt={resource.name} />
+        </Link>
+        <div className="product-list-detail">
+          <div className={styles.resourceName}>
+            <Link href={`/resources/${resource.id}`}>{resource.name}</Link>
+          </div>
+          <div className={styles.resourceValidation}>
+            <Average average={parseFloat(resource.average_evaluation).toFixed(1)} />
+          </div>
+          <div className={styles.resourceUrl}>
+            <i className="pi pi-link"></i>{' '}
+            <a href={resource.url} target="_blank" rel="noreferrer">
+              Enlace del recurso
+            </a>
+          </div>
+          <div className={styles.resourceLink}>
+            <Link href={`/resources/${resource.id}`}>
+              <Button label="Ver recurso" icon="pi pi-book" className="p-button-outlined p-button-sm" />
+            </Link>
           </div>
         </div>
       </div>
