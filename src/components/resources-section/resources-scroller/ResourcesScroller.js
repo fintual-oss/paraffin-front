03773f931df2116f9abd266aa677069ccc180dcb
Pablo Abara <pbabara@uc.cl>
2022-10-21 10:19:42 -0300

    feat(resources-scroll-display): change style to avoid overlapping

--- src/components/resources-section/resources-scroller/ResourcesScroller.js
+++ src/components/resources-section/resources-scroller/ResourcesScroller.js
@@ -1,10 +1,11 @@
 import { DataScroller } from 'primereact/datascroller';
 import ResourcesListItem from '../resources-list-item/ResourcesListItem';
+import styles from './ResourceScroller.module.scss';
 
 const ResourcesScroller = ({ resources }) => {
   const itemTemplate = (resource) => <ResourcesListItem resource={resource} />;
 
-  return <DataScroller value={resources} itemTemplate={itemTemplate} rows={10} inline />;
+  return <DataScroller className={styles.myScroller} value={resources} itemTemplate={itemTemplate} rows={10} />;
 };
 
 export default ResourcesScroller;
