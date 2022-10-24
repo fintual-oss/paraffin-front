af0307acbf47e6dc18319977f0bd03f462baefee
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-24 14:22:15 -0300

    chore(linter): ESLint & prettier max-len 80

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,15 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+  ],
   rules: {
     semi: ['error', 'always'],
+    'max-len': ['error', { code: 80, ignoreUrls: true, ignoreStrings: true }],
   },
 };
