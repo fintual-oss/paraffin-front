f9c0be4abd2d6186fc9a93952068b36105bff90e
ristofer <cristopher.gomez@ug.uchile.cl>
2022-10-24 10:57:12 -0300

    feat(resource-sidebar): add styles to ResourceSidebar component

--- src/components/resources-section/resource-sidebar/ResourceSidebar.js
+++ src/components/resources-section/resource-sidebar/ResourceSidebar.js
@@ -2,10 +2,11 @@ import { Sidebar } from 'primereact/sidebar';
 import LinkButton from '@components/learning-units-section/link-button/LinkButton';
 import { Card } from 'primereact/card';
 import AddEvaluation from '@components/resource-section/add-evaluation/AddEvaluation';
+import styles from './ResourceSidebar.module.scss';
 
 const ResourceSidebar = ({ visible, onHideHandler, activeResource, formOptions }) => {
   return (
-    <Sidebar visible={visible} position="right" onHide={() => onHideHandler()}>
+    <Sidebar visible={visible} position="right" onHide={() => onHideHandler()} className={styles.resourceSidebar}>
       <h1>{activeResource.name}</h1>
       <Card title="Evaluación promedio">
         {parseFloat(activeResource.average_evaluation).toFixed(1)} <i class="pi pi-star-fill"></i>
