e40e183fcf11ed537ad5b4ece88d6d52143f8f8e
Carlos Riquelme <criquelme2@gmail.com>
2022-10-24 15:50:00 -0300

    feat(landing): wip solution

--- src/components/landing/solution/Solution.js
+++ src/components/landing/solution/Solution.js
@@ -1,43 +1,47 @@
-import React from 'react'
-import styles from './Solution.module.scss'
+import React from 'react';
+import styles from './Solution.module.scss';
 
 const solution = () => {
   return (
     <section className={`${styles.solution} grid`}>
       <div className={`${styles.titleSections}`}>
-        <h2 class={`${styles.textCenter} ${styles.fin_font}`}>(Solución: Paraffin...)</h2>
+        <h2 class={`${styles.textCenter} ${styles.fin_font}`}>
+          ser.dev
+        </h2>
       </div>
       <div className={`${styles.c} grid`}>
-        <div className={`col-12 ${styles.solution__card}`}>
-          <img src="https://picsum.photos/400/200" alt="Logo" />
-          <div className="column_info_card">
-            <h3 className={styles.solution__card__title}>¿Qué es Paraffin?</h3>
-            <p className={styles.solution__card__description}>
-              Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
-            </p>
+        <div class={`col-12 ${styles.solution__card_wrap}`}>
+          <div className={`col-12 xl:col-10 ${styles.solution__card}`}>
+            <img src="https://picsum.photos/400/200" alt="Logo" />
+            <div className={styles.column_info_card_r}>
+              <h3 className={styles.solution__card__title}>¿Qué es Paraffin?</h3>
+              <p className={styles.solution__card__description}>
+                Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className={`col-12 ${styles.solution__card}`}>
-          <div className={`${styles.column_info_card}, ${styles.textRight}`}>
-            <h3 className={`${styles.solution__card__title}`}>¿Qué es Paraffin?</h3>
-            <p className={`${styles.solution__card__description} textCenter`}>
-              Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
-            </p>
+          <div className={`col-12 xl:col-10 ${styles.solution__card}`}>
+            <div className={`${styles.column_info_card_l} ${styles.textRight}`}>
+              <h3 className={`${styles.solution__card__title}`}>¿Qué es Paraffin?</h3>
+              <p className={styles.solution__card__description}>
+                Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
+              </p>
+            </div>
+            <img src="https://picsum.photos/400/200" alt="Logo" />
           </div>
-          <img src="https://picsum.photos/400/200" alt="Logo" />
+          <div className={`col-12 xl:col-10 ${styles.solution__card}`}>
+            <img src="https://picsum.photos/400/200" alt="Logo" />
+            <div className={styles.column_info_card_r}>
+              <h3 className={styles.solution__card__title}>¿Qué es Paraffin?</h3>
+              <p className={styles.solution__card__description}>
+                Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={`col-12 ${styles.solution__card}`}>
-          <img src="https://picsum.photos/400/200" alt="Logo" />
-          <div className="column_info_card">
-            <h3 className={styles.solution__card__title}>¿Qué es Paraffin?</h3>
-            <p className={styles.solution__card__description}>
-              Paraffin es una plataforma educativa que busca ayudar a los jóvenes a encontrar su vocación y a los profesionales a encontrar su próximo talento.
-            </p>
-          </div>
-        </div>
     </section>
-  )
-}
+  );
+};
 
-export default solution
+export default solution;
