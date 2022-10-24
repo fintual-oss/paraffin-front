c9608c0fcecadc58699ceaa2cb2cc8991a9ebf94
Carlos Riquelme <criquelme2@gmail.com>
2022-10-24 15:49:33 -0300

    feat(landing): wip header

--- src/components/landing/header/Header.js
+++ src/components/landing/header/Header.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import styles from './Header.module.scss'
+import React from 'react';
+import styles from './Header.module.scss';
 
-import { Button } from 'primereact/button'
-import Image from 'next/image'
+import { Button } from 'primereact/button';
+import Image from 'next/image';
 import developer_header from '@utils/images/developer_header.png';
 
 const Header = () => {
   return (
     <section id={styles.header}>
       <ul className={styles.circles}>
-        {[...Array(10)].map((i) => <li key={i} /> )}
+        {[...Array(10)].map((i) => (
+          <li key={i} />
+        ))}
       </ul>
       <div className={`col-12 md:col-6 ${styles.header__content}`}>
-        <h1 className={styles.header__title}>(Título...)</h1>
+        <h1 className={styles.header__title}>Tu ruta para ser.dev</h1>
         <p className={styles.header__description}>
-          Praesent scelerisque lorem arcu, a scelerisque ante varius sit amet. Nunc condimentum   pellentesque arcu luctus eleifend. Phasellus accumsan porta volutpat. 
+        Sabes programar pero te falta algo más para ser.dev, con nuestra guía y el apoyo de la comunidad lo lograrás.
         </p>
         <Button label="Quiero registrarme en Paraffin" />
       </div>
@@ -22,7 +24,7 @@ const Header = () => {
         <Image src={developer_header} alt="Logo" id={styles.header__logo} />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
