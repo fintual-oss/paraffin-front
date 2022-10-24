094916094881be35feb0a1a04e99f987d890fc98
Juan Santos <jjsantosochoa@outlook.com>
2022-10-24 15:04:43 -0300

    feat(unauth_complete_lu): show modal when click on disabed

--- src/components/learning-units-section/complete-learning-unit-toogle/CompleteLearningUnitNoAuth.js
+++ src/components/learning-units-section/complete-learning-unit-toogle/CompleteLearningUnitNoAuth.js
@@ -1,12 +1,21 @@
-import Link from 'next/link';
 import { CompleteLearningUnitToggle } from './CompleteLearningUnitToogle';
+import { Dialog } from 'primereact/dialog';
+import { useState } from 'react';
 
 export const CompleteLearningUnitToggleNoAuth = () => {
+  const [displayDialog, setDisplayDialog] = useState(false);
+  const onHide = () => {
+    setDisplayDialog(false);
+  };
   return (
-    <Link href="/users/sign_in">
-      <div>
+    <div>
+      <div onClick={() => setDisplayDialog(true)}>
         <CompleteLearningUnitToggle className="p-disabled" tooltip="Necesita ingresar para completar" />
       </div>
-    </Link>
+
+      <Dialog visible={displayDialog} style={{ width: '50vw' }} onHide={onHide}>
+        <p> Paraffin se construye en comunidad </p>
+      </Dialog>
+    </div>
   );
 };
