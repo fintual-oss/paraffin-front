f0e2d790b39d181b01e7caecbb0aefe36c9d0972
ristofer <cristopher.gomez@ug.uchile.cl>
2022-10-24 12:09:41 -0300

    fix(resource-page): use the Average component on ResourceSidebar to handle null evaluations

--- src/components/resources-section/resource-sidebar/ResourceSidebar.js
+++ src/components/resources-section/resource-sidebar/ResourceSidebar.js
@@ -1,7 +1,8 @@
 import { Sidebar } from 'primereact/sidebar';
-import LinkButton from '@components/resource-section/link-button/LinkButton';
 import { Card } from 'primereact/card';
+import LinkButton from '@components/resource-section/link-button/LinkButton';
 import AddEvaluation from '@components/resource-section/add-evaluation/AddEvaluation';
+import Average from '@components/resources-section/average/Average';
 import styles from './ResourceSidebar.module.scss';
 
 const ResourceSidebar = ({ visible, onHideHandler, activeResource, formOptions }) => {
@@ -9,7 +10,7 @@ const ResourceSidebar = ({ visible, onHideHandler, activeResource, formOptions }
     <Sidebar visible={visible} position="right" onHide={() => onHideHandler()} className={styles.resourceSidebar}>
       <h1>{activeResource.name}</h1>
       <Card title="Evaluación promedio">
-        {parseFloat(activeResource.average_evaluation).toFixed(1)} <i class="pi pi-star-fill"></i>
+        <Average average={activeResource.average_evaluation} />
         <LinkButton url={activeResource.url} />
       </Card>
       <AddEvaluation formOptions={formOptions} />
