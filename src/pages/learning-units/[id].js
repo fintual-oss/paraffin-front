9b5cffcdde92d6651e68658ca62977f1f0320cdb
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-26 12:34:30 -0300

    feat(ssr): add getserversideprops function for ssr

--- src/pages/learning-units/[id].js
+++ src/pages/learning-units/[id].js
@@ -1,15 +1,37 @@
-import { useRouter } from 'next/router';
 import ResourcesSection from '@components/resources-section/resources-section/ResourcesSection';
-import { Skeleton } from 'primereact/skeleton';
+import { endpoints } from '@utils/endpoints';
 
-const LearningUnitPage = () => {
-  const { query, isReady } = useRouter();
-  const learningUnitId = query.id;
-
-  if (!isReady)
-    return <Skeleton shape="rectangle" width="100%" height="100%" />;
-
-  return <ResourcesSection learningUnitId={learningUnitId} />;
+const LearningUnitPage = (props) => {
+  return (
+    <ResourcesSection
+      learningUnit={props.learningUnit}
+      resources={props.resources}
+      isError={props.isError}
+    />
+  );
 };
 
 export default LearningUnitPage;
+
+export async function getServerSideProps(context) {
+  const learningUnitId = context.query.id;
+
+  const resUnit = await fetch(endpoints('learningUnit', learningUnitId));
+  const learningUnit = await resUnit.json();
+
+  const resResource = await fetch(
+    endpoints('learningUnitResources', learningUnitId)
+  );
+  const resources = await resResource.json();
+  const isErrorUnit = learningUnit ? null : true;
+  const isErrorResources = resources ? null : true;
+  const isError = isErrorUnit || isErrorResources;
+
+  return {
+    props: {
+      learningUnit: learningUnit,
+      resources: resources,
+      isError: isError,
+    },
+  };
+}
