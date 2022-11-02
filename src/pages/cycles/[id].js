8cdc554a5a947673ddadad70fc11dabca6e26285
alejandroleiva <aleivailabaca@gmail.com>
2022-11-02 11:50:08 -0300

    feat(cycle-view): create cycles page

---
+++ src/pages/cycles/[id].js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import CycleSection from '@components/cycle-section/cycle-section/CycleSection';
+import { Skeleton } from 'primereact/skeleton';
+
+const CyclePage = () => {
+  const { query, isReady } = useRouter();
+
+  if (!isReady)
+    return <Skeleton shape="rectangle" width="100%" height="100%" />;
+
+  const cycleId = query.id;
+
+  if (process.env.NODE_ENV === 'development'){
+    return (
+      <>
+        <CycleSection cycleId={cycleId}/>
+      </>
+      );
+    } else {
+      return <p>WIP</p>
+    }
+}
+
+export default CyclePage;
