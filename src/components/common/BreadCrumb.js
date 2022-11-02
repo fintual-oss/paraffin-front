13bb73435cc6650941c556caa432edb817b53fcd
alejandroleiva <aleivailabaca@gmail.com>
2022-11-02 15:28:20 -0300

    feat(cycle-view): create components/common directory and BreadCrumb component

---
+++ src/components/common/BreadCrumb.js
@@ -0,0 +1,16 @@
+import { BreadCrumb } from 'primereact/breadcrumb';
+import styles from './BreadCrumb.module.scss'
+
+export const CycleBreadCrumb = ({ cycle }) => {
+  const items = [
+    { label: 'Curriculum', url: '/curriculums/1' },
+    { label: cycle.name }
+  ];
+
+  const home = { icon: 'pi pi-home', url: '/' }
+
+  return <BreadCrumb
+    className={styles.cycleBreadCrumb}
+    model={items}
+    home={home}/>
+}
