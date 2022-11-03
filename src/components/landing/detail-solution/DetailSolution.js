e96a1749c4f863104d333fba1912fa6df3d29ab9
cecyramallo <cecyramallo@gmail.com>
2022-11-03 17:39:07 -0300

    fix(landing): delete comments

--- src/components/landing/detail-solution/DetailSolution.js
+++ src/components/landing/detail-solution/DetailSolution.js
@@ -3,10 +3,7 @@ import styles from './DetailSolution.module.scss';
 
 const DetailSolution = () => {
   return (
-    <section className={`${styles.detailSolution} grid`}>
-      {/* <p class="textCenter">(Detalle de la solución)</p>
-      <p class="textCenter">(Call to action)</p> */}
-    </section>
+    <></>
   );
 };
 
