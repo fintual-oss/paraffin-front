7d56fbea1d83962af3fdcb30ef23cc424153c121
Pablo Abara <pbabara@uc.cl>
2022-11-03 17:19:58 -0300

    feat(resource-label): add resource label component

--- src/components/resources-section/resources-list-item/ResourcesListItem.js
+++ src/components/resources-section/resources-list-item/ResourcesListItem.js
@@ -4,13 +4,21 @@ import styles from './ResourcesListItem.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import profilePic from '@utils/images/resource.jpg';
+import { Tag } from 'primereact/tag';
 
 const ResourcesListItem = ({ resource, resourceViewButtonHandler }) => {
+  const displayTags = () => {
+    if (resource.resource_labels.length > 0) return '';
+    console.log(resource);
+    return <Tag icon={'pi ' + resource.resource_labels.icon}></Tag>;
+  };
+
   return (
     <div>
       <div className={styles.resourceGridItem}>
         <div className={styles.resourceListItem}>
           <Image className={styles.img} src={profilePic} alt={resource.name} />
+          <div>{displayTags()}</div>
           <div>
             <div className={styles.resourceName}>
               <Link href={`/resources/${resource.id}`}>{resource.name}</Link>
