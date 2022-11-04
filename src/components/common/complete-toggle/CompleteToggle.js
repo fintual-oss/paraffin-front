1728437155b8118230a3e8769fa17d5dd6aebd59
ristofer <cristopher.gomez@ug.uchile.cl>
2022-11-04 12:30:22 -0300

    feat(completed-resources): delete style on CompleteToggle component

--- src/components/common/complete-toggle/CompleteToggle.js
+++ src/components/common/complete-toggle/CompleteToggle.js
@@ -1,17 +1,14 @@
 import { ToggleButton } from 'primereact/togglebutton';
-import styles from './CompleteToggle.module.scss';
 
 export const CompleteToggle = ({ completed, onChangeHandler }) => {
   return (
-    <div className={styles.checkbox}>
-      <ToggleButton
-        onLabel="Completado"
-        offLabel="No Completado"
-        onIcon="pi pi-check"
-        offIcon="pi pi-times"
-        checked={completed}
-        onChange={onChangeHandler}
-      />
-    </div>
+    <ToggleButton
+      onLabel="Completado"
+      offLabel="No Completado"
+      onIcon="pi pi-check"
+      offIcon="pi pi-times"
+      checked={completed}
+      onChange={onChangeHandler}
+    />
   );
 };
