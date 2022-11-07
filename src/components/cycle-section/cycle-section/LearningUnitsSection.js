2a59c967e02cc21d39995aa3558aaf2213330a6a
Ronald Tichauer <rtichauerv@gmail.com>
2022-11-06 22:02:11 -0300

    refactor(LearningUnitsSection): List and graph

---
+++ src/components/cycle-section/cycle-section/LearningUnitsSection.js
@@ -0,0 +1,39 @@
+import { TabView, TabPanel } from 'primereact/tabview';
+import LearningUnitsListTab from '@components/cycle-section/learning-units-section/learning-units-section/LearningUnitsListTab';
+import LearningUnitsGraph from '@components/cycle-section/learning-units-section/graph-section/LearningUnitsGraph';
+
+const LearningUnitsSection = ({
+  learningUnits,
+  successions,
+  handleLearningUnitClick,
+}) => {
+  return (
+    <TabView>
+      <TabPanel header="Mapa de pre requisitos">
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          <div
+            style={{
+              height: 600,
+              width: '89.6vw',
+              margin: 15,
+              position: 'relative',
+            }}
+          >
+            <LearningUnitsGraph
+              handleNodeClick={(id, isCompleted) =>
+                handleLearningUnitClick(id, isCompleted)
+              }
+              learningUnits={learningUnits}
+              successions={successions}
+            />
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel header="Lista de unidades">
+        <LearningUnitsListTab learningUnits={learningUnits} />
+      </TabPanel>
+    </TabView>
+  );
+};
+
+export default LearningUnitsSection;
