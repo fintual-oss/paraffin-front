cc9f3e36b49240f2c73053153fbf22ac79225a9d
Ronald Tichauer <rtichauerv@gmail.com>
2022-11-08 18:35:16 -0300

    refactor(resources-sorted): sort learning unit's resources by average_evaluation

--- src/components/resources-section/resources-scroller/ResourcesScroller.js
+++ src/components/resources-section/resources-scroller/ResourcesScroller.js
@@ -3,6 +3,9 @@ import ResourcesListItem from '../resources-list-item/ResourcesListItem';
 import styles from './ResourceScroller.module.scss';
 
 const ResourcesScroller = ({ resources, resourceViewButtonHandler }) => {
+  resources.sort((a, b) =>
+    b.average_evaluation > a.average_evaluation ? 1 : -1
+  );
   const itemTemplate = (resource) => (
     <ResourcesListItem
       resource={resource}
