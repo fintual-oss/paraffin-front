70566db22979cb569ada4807600f631df8a470b0
Ronald Tichauer <rtichauerv@gmail.com>
2022-11-09 12:16:14 -0300

    refactor(link-button-component): make it a common component, with label as a prop

--- src/components/common/link-button/LinkButton.js
+++ src/components/common/link-button/LinkButton.js
@@ -1,13 +1,13 @@
 import { Button } from 'primereact/button';
 import styles from './LinkButton.module.scss';
 
-const LinkButton = ({ url }) => {
+const LinkButton = ({ url, label }) => {
   if (!url) return <></>;
 
   return (
     <p>
       <a className={styles.link} href={url} target="_blank" rel="noreferrer">
-        <Button icon="pi pi-external-link" label={'Ir a recurso'}></Button>
+        <Button icon="pi pi-external-link" label={label}></Button>
       </a>
     </p>
   );
