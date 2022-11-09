eed97ee40df96fa607524bc1e91a0209016cc6c0
Ronald Tichauer <rtichauerv@gmail.com>
2022-11-09 16:52:30 -0300

    fix(graph-font): new font

--- src/components/cycle-section/learning-units-section/graph/Graph.js
+++ src/components/cycle-section/learning-units-section/graph/Graph.js
@@ -42,6 +42,7 @@ const Graph = ({ nodes, edges, theme, nodePredecessors, handleNodeClick }) => {
         onNodeClick={(event) => handleNodeClick(event.id)}
         onNodePointerOver={(event) => handleNodePointOver(event)}
         onNodePointerOut={() => setSelections([])}
+        labelFontUrl="https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff"
       />
     </div>
   );
